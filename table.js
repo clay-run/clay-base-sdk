319c11018056580d870ff218fdee2cca43d8d602
Théo Szymkowiak <theoszymko@me.com>
2017-11-20 22:09:24 -0500

    Typo

--- table.js
+++ table.js
@@ -1,5 +1,5 @@
 module.exports = function Table(tableName, db) {
     this.findAll = function() {
-        return db.query(`SELECT * FROM ${tablename}`);
+        return db.query(`SELECT * FROM ${tableName}`);
     }
 }
