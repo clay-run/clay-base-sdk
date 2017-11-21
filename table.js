dd94ef5bae6a994c3f1f1bcfd1991e32f8586322
Théo Szymkowiak <theoszymko@me.com>
2017-11-21 10:05:02 -0500

    Adding find, insert, findAll

--- table.js
+++ table.js
@@ -1,5 +1,23 @@
 module.exports = function Table(tableName, db) {
     this.findAll = function() {
-        return db.query(`SELECT * FROM ${tableName}`);
+        return db.api.post(`/bases/find`).send({
+            table: tableName
+        });
     }
+
+    this.insert = function(values) {
+        return db.api.post(`/bases/insert`).send({
+            table: tableName,
+            values: values
+        });
+    }
+
+    this.find = function(config) {
+        return db.api.post(`/bases/find`).send({
+            table: tableName,
+            config: config
+        })
+    }
+
+    this.query = db.query;
 }
